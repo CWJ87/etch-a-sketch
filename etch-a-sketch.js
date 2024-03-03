e36c816218f6c6382c380783a13ed45005f9ed48
Culbert Willoughby <culbertw@gmail.com>
2024-03-03 16:40:59 -0600

    feat: add function for creating 16x16 grid
    - create16x16Grid function creates the 16x16 grid for display.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,2 +1,26 @@
 "use strict";
 
+// create a container for the grid
+// create 16x16 grid
+function create16x16Grid() {
+    const TOTAL_ROWS = 16;
+    const TOTAL_SQUARES_PER_ROW = 16;
+    const container = document.createElement("div");
+
+    // each row will have 16 boxes
+    for (let rowCnt = 0; rowCnt < TOTAL_ROWS; rowCnt++) {
+        const row = document.createElement("div");
+
+        for (let boxCnt = 0; boxCnt < TOTAL_SQUARES_PER_ROW; boxCnt++) {
+            const box = document.createElement("div");
+            row.appendChild(box);
+        }
+
+        // add the newly created row to the container
+        container.appendChild(row);
+    }
+
+    document.body.appendChild(container);
+}
+
+create16x16Grid();
